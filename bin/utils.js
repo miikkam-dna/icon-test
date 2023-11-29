f3c65ac501793070605c4d0d3011d671e9a638a1
miikkam-dna <148067964+miikkam-dna@users.noreply.github.com>
2023-11-29 14:13:08 +0200

    Update utils.js

--- bin/utils.js
+++ bin/utils.js
@@ -1,7 +1,7 @@
 const upperCamelCase = require('uppercamelcase')
 
 const parseName = (name, defaultStyle) => {
-  const cleanedName = name.replace(/\//g, '-') // Replace "/" with "-"
+  const cleanedName = name.replace(/\//g, '_') // Replace "/" with "_"
   const nameSlices = cleanedName.split('/')
   const iconName = nameSlices[nameSlices.length - 1]
   const style = iconName.split('-').pop()
